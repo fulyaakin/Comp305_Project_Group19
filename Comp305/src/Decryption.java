6b212419faf0dc4a2684d5f330dca1371c0a9fff
Fulya Akın <fulyaakin@Fulyas-MacBook-Pro.local>
2021-05-27 13:10:11 +0300

    graph with adjacency list is added.

--- Comp305/src/Decryption.java
+++ Comp305/src/Decryption.java
@@ -55,33 +55,3 @@ class DecryptionGraph
 	}
 }
 
-
-public class Main {
-
-	public static void main(String[] args) {
-		int x = 9;
-		int y = 5;
-		DecryptionGraph dg = new DecryptionGraph(26);
-		dg.connect((int) 'a' -97, (int) 'b' -97);
-		dg.connect((int) 'b' -97, (int) 'c' -97);
-		dg.connect((int) 'c' -97, (int) 'd' -97);
-		dg.connect((int) 'd' -97, (int) 'e' -97);
-		dg.connect((int) 'e' -97, (int) 'f' -97);
-		dg.connect((int) 'f' -97, (int) 'g' -97);
-		dg.connect((int) 'g' -97, (int) 'h' -97);
-		dg.connect((int) 'h' -97, (int) 'i' -97);
-		dg.connect((int) 'i' -97, (int) 'j' -97);
-		dg.connect((int) 'j' -97, (int) 'z' -97);
-		
-		char x1 = 'a';
-		char y1 = 'd';
-		if (dg.convertable((int) x1 -97, (int) y1 -97))
-			System.out.println( x1 + " can be convertable to " + y1);
-		else
-			System.out.println(x1 + " can not be convertable to " + y1);
-
-
-	}
-
-}
-
